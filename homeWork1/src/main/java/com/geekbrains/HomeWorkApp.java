04c337a4ef2dbde90f41bb3e09d20c82345423a5
Anatoly <glenglen74@gmail.com>
2021-12-21 07:39:47 +0500

    test

--- homeWork1/src/main/java/com/geekbrains/HomeWorkApp.java
+++ homeWork1/src/main/java/com/geekbrains/HomeWorkApp.java
@@ -2,6 +2,13 @@ package com.geekbrains;
 
 public class HomeWorkApp {
     public static void main(String[] args){
-        
+        int c = 1;
+        int c1 = 1;
+        int c2 = 1;
+        int c3 = 1;
+        int c4 = 1;
+        int c5 = 1;
+        int c6 = 1;
+        int c7 = 1;
     }
 }
